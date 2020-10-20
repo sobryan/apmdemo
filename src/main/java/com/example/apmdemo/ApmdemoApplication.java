de997e3b2db6826ec8411b54ca31478a371a445c
sobryan <stobryan@gmail.com>
2020-10-20 13:54:53 -0500

    initial

---
+++ src/main/java/com/example/apmdemo/ApmdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmdemoApplication.class, args);
+	}
+
+}
